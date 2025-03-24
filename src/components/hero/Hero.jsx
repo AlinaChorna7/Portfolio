d860c787254e9b9904e43a71fd7bef794f614838
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2025-03-24 17:25:07 +0100

    pre-final styles upd

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,8 +10,8 @@ return(
             <h1 className="hero-title">Hi! I`m Alina Chorna, Full-Stack JavaScript Developer. </h1>
             <h3 className="hero-text">Turning Ideas Into Functional Code.</h3>
         </div>
-        <div className='port-photo'>
-            <img  className="portfolio-image" src='/images/zdjecia-do-cv.png'/>
+        <div className='portfolio-image'>
+            {/* <img  className="portfolio-image" src='/images/portfolio-photo.jpg'/> */}
         </div>
 
     </section>
